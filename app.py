b9aab91648a3205220468fd0715ccf83282e6114
hyesoo kim <khwater1017@naver.com>
2022-10-03 01:30:56 +0900

    코사인 유사도

--- app.py
+++ app.py
@@ -101,13 +101,12 @@ for i in range(0, get_files_count(wavpath)):
     loud = classifyLoudness("./data/jsonfile/sit" + str(i + 1) + ".json")
     loudList.append(loud)
 
-
 # 스케일링 : 정규화
 mfcc_np = np.array(mfccList)
 print(mfcc_np.shape)
 
 # 분류기에서 사용하기 위해 3차원 벡터를 2차원 벡터로 변환
-mfcc_np = mfcc_np.reshape((100, 40*80))
+mfcc_np = mfcc_np.reshape((100, 40 * 80))
 
 # 정규화: 추출한 벡터를 0~1 사이의 값으로 변환
 scaler = MinMaxScaler()
@@ -125,3 +124,46 @@ for i in range(0, get_files_count(wavpath)):
     # object를 배열에 삽입
     processMFCCList.append(object_mfcc)
 
+
+#코사인 유사도 시작
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
+
+#코사인유사도 함수
+def cosine_similarity(A, B):
+    return dot(A, B) / (norm(A) * norm(B))
+
+#print(len(processMFCCList)) 출력할 MFCC길이 확인
+
+#배열 선언
+voicesimilarity=[] #유사도를 담은 배열
+processSimilarityList=[] #VOICESIM 객체를 담은 배열
+
+#유사도 구하는 반복문
+for i in range(100):
+    print(f"{i}. 문서1-문서2 간 유사도: {cosine_similarity(processMFCCList[i].mfcc, processMFCCList[0].mfcc)}")
+    voicesimilarity.append(cosine_similarity(processMFCCList[i].mfcc, processMFCCList[0].mfcc))
+
+#VOICESIM 클래스 형식
+class VOICESIM:
+    # 속성 생성
+    def __init__(self, name, rapidness, loudness, similarity):
+        self.name = name,
+        self.rapidness = rapidness,
+        self.loudness = loudness,
+        self.similarity = similarity
+
+# voicesim 객체 삽입
+for i in range(0, 100):
+    name = "sit" + str(i + 1) + ".wav"
+
+    # 코사인유사도 + 정보(loud, rapid) 담긴 class 생성
+    object_voicesim = VOICESIM(name, rapidList[i], loudList[i], voicesimilarity[i])
+
+    # object를 배열에 삽입
+    processSimilarityList.append(object_voicesim)
+
+#for i in range(100):
+#    print(processSimilarityList[i].similarity)
+#제대로 들어있는지 확인!
